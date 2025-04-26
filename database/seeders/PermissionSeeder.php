98d49ada325119ee9e0e82eb97c692bee12c26b8
Hanter Duarte <hanterduarte@gmail.com>
2025-04-26 18:58:43 -0300

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -10,6 +10,7 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
+            // Permissões de Usuários
             [
                 'name' => 'Visualizar Usuários',
                 'slug' => 'view-users',
@@ -30,6 +31,8 @@ class PermissionSeeder extends Seeder
                 'slug' => 'delete-users',
                 'description' => 'Permite excluir usuários'
             ],
+            
+            // Permissões de Sistema
             [
                 'name' => 'Gerenciar Permissões',
                 'slug' => 'manage-permissions',
@@ -45,21 +48,126 @@ class PermissionSeeder extends Seeder
                 'slug' => 'view-reports',
                 'description' => 'Permite visualizar relatórios do sistema'
             ],
+            
+            // Permissões de Serviços
             [
-                'name' => 'Gerenciar Agendamentos',
-                'slug' => 'manage-appointments',
-                'description' => 'Permite gerenciar agendamentos'
+                'name' => 'Visualizar Serviços',
+                'slug' => 'view-services',
+                'description' => 'Permite visualizar serviços'
             ],
             [
-                'name' => 'Gerenciar Serviços',
-                'slug' => 'manage-services',
-                'description' => 'Permite gerenciar serviços oferecidos'
+                'name' => 'Criar Serviços',
+                'slug' => 'create-services',
+                'description' => 'Permite criar novos serviços'
             ],
             [
-                'name' => 'Gerenciar Clientes',
-                'slug' => 'manage-clients',
-                'description' => 'Permite gerenciar clientes'
-            ]
+                'name' => 'Editar Serviços',
+                'slug' => 'edit-services',
+                'description' => 'Permite editar serviços existentes'
+            ],
+            [
+                'name' => 'Excluir Serviços',
+                'slug' => 'delete-services',
+                'description' => 'Permite excluir serviços'
+            ],
+            
+            // Permissões de Agendamentos
+            [
+                'name' => 'Visualizar Agendamentos',
+                'slug' => 'view-appointments',
+                'description' => 'Permite visualizar agendamentos'
+            ],
+            [
+                'name' => 'Criar Agendamentos',
+                'slug' => 'create-appointments',
+                'description' => 'Permite criar novos agendamentos'
+            ],
+            [
+                'name' => 'Editar Agendamentos',
+                'slug' => 'edit-appointments',
+                'description' => 'Permite editar agendamentos existentes'
+            ],
+            [
+                'name' => 'Excluir Agendamentos',
+                'slug' => 'delete-appointments',
+                'description' => 'Permite excluir agendamentos'
+            ],
+            
+            // Permissões de Produtos
+            [
+                'name' => 'Visualizar Produtos',
+                'slug' => 'view-products',
+                'description' => 'Permite visualizar produtos'
+            ],
+            [
+                'name' => 'Criar Produtos',
+                'slug' => 'create-products',
+                'description' => 'Permite criar novos produtos'
+            ],
+            [
+                'name' => 'Editar Produtos',
+                'slug' => 'edit-products',
+                'description' => 'Permite editar produtos existentes'
+            ],
+            [
+                'name' => 'Excluir Produtos',
+                'slug' => 'delete-products',
+                'description' => 'Permite excluir produtos'
+            ],
+            
+            // Permissões de Clientes
+            [
+                'name' => 'Visualizar Clientes',
+                'slug' => 'view-clients',
+                'description' => 'Permite visualizar clientes'
+            ],
+            [
+                'name' => 'Criar Clientes',
+                'slug' => 'create-clients',
+                'description' => 'Permite criar novos clientes'
+            ],
+            [
+                'name' => 'Editar Clientes',
+                'slug' => 'edit-clients',
+                'description' => 'Permite editar clientes existentes'
+            ],
+            [
+                'name' => 'Excluir Clientes',
+                'slug' => 'delete-clients',
+                'description' => 'Permite excluir clientes'
+            ],
+            
+            // Permissões de PDV
+            [
+                'name' => 'Abrir Caixa',
+                'slug' => 'open-cash-register',
+                'description' => 'Permite abrir o caixa'
+            ],
+            [
+                'name' => 'Fechar Caixa',
+                'slug' => 'close-cash-register',
+                'description' => 'Permite fechar o caixa'
+            ],
+            [
+                'name' => 'Fazer Sangria',
+                'slug' => 'make-withdrawal',
+                'description' => 'Permite fazer sangria do caixa'
+            ],
+            [
+                'name' => 'Fazer Reforço de Caixa',
+                'slug' => 'make-reinforcement',
+                'description' => 'Permite fazer reforço de caixa'
+            ],
+            [
+                'name' => 'Fazer Venda',
+                'slug' => 'make-sale',
+                'description' => 'Permite realizar vendas'
+            ],
+            [
+                'name' => 'Permite Desconto',
+                'slug' => 'allow-discount',
+                'description' => 'Permite aplicar descontos nas vendas'
+            ],
         ];
 
         foreach ($permissions as $permission) {
